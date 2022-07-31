ed5668e8278b35a1fda3dd0c5587f51ca7394020
Mohd Tazammul <tazammul1683@gmail.com>
2022-07-31 23:48:21 +0530

    Successfully converted into React app

---
+++ src/components/Main.jsx
@@ -0,0 +1,52 @@
+import React from 'react'
+import './style.css';
+function Main() {
+    return (
+        <div class="outer-box">
+            <div class="wheel1">
+                <div class="wheel-outer1"></div>
+                <div class="wheel-outer2"></div>
+                <div class="subWheel11">
+                </div>
+                <div class="subWheel12">
+                </div>
+                <div class="chain-1"></div>
+                <div class="chain-2"></div>
+                <div class="moving-wheel">
+                </div>
+                <div class="middle-wheel">
+
+                    <div class="stick1"></div>
+                    <div class="stick2"></div>
+                    <div class="middle-sub-wheel"></div>
+                    <div class="paddle"></div>
+                    <div class="sub-paddle"></div>
+                </div>
+            </div>
+            <div class="wheel2">
+                <div class="wheel-outer1"></div>
+                <div class="wheel-outer2"></div>
+                <div class="subWheel11">
+                </div>
+                <div class="subWheel12">
+                </div>
+                <div class="moving-wheel">
+                </div>
+                <div class="stick3"></div>
+            </div>
+            <div class="stick4"></div>
+            <div class="seat">
+                <div></div>
+                <div></div>
+            </div>
+            <div class="handle">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+        </div>
+    )
+}
+
+export default Main
