c9b0dbca6acf9d775438ed0e447bd9cfc2409e5e
Mohd Tazammul <tazammul1683@gmail.com>
2022-08-01 00:53:06 +0530

    Finally covered all the aspects

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,50 +2,71 @@ import React from 'react'
 import './style.css';
 function Main() {
     return (
-        <div class="outer-box">
-            <div class="wheel1">
-                <div class="wheel-outer1"></div>
-                <div class="wheel-outer2"></div>
-                <div class="subWheel11">
+        <>
+        <div className='clouds'>
+            <div> <marquee scrollamount={"20"}><img src='https://www.pngarts.com/files/10/Vector-Cloud-Outline-PNG-Picture.png' height={"100px"} /></marquee></div>
+            <div> <marquee scrollamount={"30"}><img src='https://www.pngarts.com/files/10/Vector-Cloud-Outline-PNG-Picture.png' height={"150px"} /></marquee></div>
+            <div> <marquee scrollamount={"15"}><img src='https://www.pngarts.com/files/10/Vector-Cloud-Outline-PNG-Picture.png' height={"120px"} /></marquee></div>
+            <div> <marquee scrollamount={"35"}><img src='https://www.pngarts.com/files/10/Vector-Cloud-Outline-PNG-Picture.png' height={"150px"} /></marquee></div>
+            <div> <marquee scrollamount={"18"}><img src='https://www.pngarts.com/files/10/Vector-Cloud-Outline-PNG-Picture.png' height={"70px"} /></marquee></div>
+        </div>
+        <div className="outer-box">
+            <div className="wheel1">
+                <div className="wheel-outer1"></div>
+                <div className="wheel-outer2"></div>
+                <div className="subWheel11">
                 </div>
-                <div class="subWheel12">
+                <div className="subWheel12">
                 </div>
-                <div class="chain-1"></div>
-                <div class="chain-2"></div>
-                <div class="moving-wheel">
+                <div className="chain-1"></div>
+                <div className="chain-2"></div>
+                <div className="moving-wheel">
                 </div>
-                <div class="middle-wheel">
+                <div className="middle-wheel">
 
-                    <div class="stick1"></div>
-                    <div class="stick2"></div>
-                    <div class="middle-sub-wheel"></div>
-                    <div class="paddle"></div>
-                    <div class="sub-paddle"></div>
+                    <div className="stick1"></div>
+                    <div className="stick2"></div>
+                    <div className="middle-sub-wheel"></div>
+                    <div className="paddle"></div>
+                    <div className="sub-paddle"></div>
                 </div>
             </div>
-            <div class="wheel2">
-                <div class="wheel-outer1"></div>
-                <div class="wheel-outer2"></div>
-                <div class="subWheel11">
+            <div className="wheel2">
+                <div className="wheel-outer1"></div>
+                <div className="wheel-outer2"></div>
+                <div className="subWheel11">
                 </div>
-                <div class="subWheel12">
+                <div className="subWheel12">
                 </div>
-                <div class="moving-wheel">
+                <div className="moving-wheel">
                 </div>
-                <div class="stick3"></div>
+                <div className="stick3"></div>
             </div>
-            <div class="stick4"></div>
-            <div class="seat">
+            <div className="stick4"></div>
+            <div className="seat">
                 <div></div>
                 <div></div>
             </div>
-            <div class="handle">
+            <div className="handle">
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+            </div>
+            <div className='path'>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
+                <div></div>
                 <div></div>
                 <div></div>
                 <div></div>
                 <div></div>
             </div>
         </div>
+        </>
     )
 }
 
